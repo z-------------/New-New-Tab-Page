dde62344a0d74b901bdc17e9cba482d29fe67cc8
Zack Guard <zacharyguard@gmail.com>
2016-03-17 21:40:40 +0800

    Switch to Forecast.io for weather data
    Because Yahoo shut down its geo.placefinder in YQL

--- weather/weather.js
+++ weather/weather.js
@@ -36,71 +36,35 @@ var avg = function(nums){
 /* start actual weather stuff */
 
 var codeIconMap = {
-    0: "Tornado",
-    1: "Cloud-Rain",
-    2: "Cloud-Rain",
-    3: "Cloud-Lightning",
-    4: "Cloud-Lightning",
-    5: "Cloud-Rain",
-    6: "Cloud-Rain",
-    7: "Cloud-Snow",
-    8: "Cloud-Drizzle",
-    9: "Cloud-Drizzle",
-    10: "Cloud-Rain",
-    11: "Cloud-Drizzle",
-    12: "Cloud-Drizzle",
-    13: "Cloud-Snow",
-    14: "Cloud-Snow",
-    15: "Cloud-Snow",
-    16: "Cloud-Snow",
-    17: "Cloud-Hail",
-    18: "Cloud-Snow",
-    19: "Cloud-Fog",
-    20: "Cloud-Fog",
-    21: "Cloud-Fog",
-    22: "Cloud-Fog",
-    23: "Wind",
-    24: "Wind",
-    25: "Thermometer-Zero",
-    26: "Cloud",
-    27: "Cloud-Moon",
-    28: "Cloud-Sun",
-    29: "Cloud-Moon",
-    30: "Cloud-Sun",
-    31: "Moon",
-    32: "Sun",
-    33: "Moon",
-    34: "Sun",
-    35: "Hail",
-    36: "Thermometer-75",
-    37: "Cloud-Lightning",
-    38: "Cloud-Lightning",
-    39: "Cloud-Lightning",
-    40: "Cloud-Drizzle",
-    41: "Cloud-Snow",
-    42: "Cloud-Snow",
-    43: "Cloud-Snow",
-    44: "Cloud",
-    45: "Cloud-Rain",
-    46: "Cloud-Snow",
-    47: "Cloud-Rain",
-    3200: "Cloud"
+    "clear-day": "Sun",
+    "clear-night": "Moon",
+    "rain": "Cloud-Rain",
+    "snow": "Cloud-Snow",
+    "sleet": "Cloud-Snow",
+    "wind": "Wind",
+    "fog": "Cloud-Fog",
+    "cloudy": "Cloud",
+    "partly-cloudy-day": "Cloud-Sun",
+    "partly-cloudy-night": "Cloud-Moon"
 };
 
 var useImperial = false;
 
 function displayWeather(data){
-    var conditionData = data.query.results.channel.item.condition;
+    var conditionData = data.weather.currently;
 
     var tempElem = document.querySelector("#temp");
     var temperatureElem = document.querySelector("#temperature");
     var conditionElem = document.querySelector("#condition");
 
-    var temperature = Math.round((useImperial ? conditionData.temp : 5 / 9 * (conditionData.temp - 32)));
+    var temperature = conditionData.temperature;
+    if (useImperial) {
+        temperature = (temperature * 9 / 5) + 32;
+    }
 
-    tempElem.style.backgroundImage = "url(/img/weather/" + codeIconMap[Number(conditionData.code)] + ".svg)";
-    temperatureElem.textContent = temperature;
-    conditionElem.textContent = conditionData.text;
+    tempElem.style.backgroundImage = "url(/img/weather/" + codeIconMap[conditionData.icon] + ".svg)";
+    temperatureElem.textContent = Math.round(temperature).toString();
+    conditionElem.textContent = conditionData.summary;
 
     document.body.classList.add("visible");
 }
@@ -132,6 +96,6 @@ chrome.storage.sync.get("useFahrenheit", function (r) {
 
 document.body.addEventListener("click", function(){
     chrome.tabs.create({
-        url: "https://weather.yahoo.com/"
+        url: "http://forecast.io/"
     });
 });
