266668f9b8590d147da04fce850677df0e7b6df3
Zachary Guard <zacharyguard@gmail.com>
2017-11-09 21:31:11 +0800

    More complete i18n-ization

--- i18n.js
+++ i18n.js
@@ -6,3 +6,17 @@ for (let i = 0, l = i18nElems.length; i < l; i++) {
     elem.textContent = message;
   }
 }
+
+let i18nTemplateElems = document.querySelectorAll("[data-msg-template]");
+for (let i = 0, l = i18nTemplateElems.length; i < l; i++) {
+  let elem = i18nTemplateElems[i];
+  let messageKeys = elem.dataset.msgTemplate.match(/\${\w+}/g).map(function(str) { return str.substring(2, str.length - 1) });
+  var finalString = elem.dataset.msgTemplate;
+  for (let j = 0, m = messageKeys.length; j < m; j++) {
+    let messageKey = messageKeys[j];
+    let message = chrome.i18n.getMessage(messageKey);
+    let pattern = new RegExp("\\${" + messageKey + "}", "g");
+    finalString = finalString.replace(pattern, message);
+  }
+  elem.textContent = finalString;
+}
