bc444cd1b5f4bd426a2da07370326132cda327b9
Zachary Guard <zacharyguard@gmail.com>
2017-11-16 20:09:58 +0800

    Exclude js-unminified dir from dist zip

--- bin/dist.sh
+++ bin/dist.sh
@@ -3,4 +3,4 @@
 cd $(dirname $(readlink -f $0))
 cd ..
 
-zip -r New-New-Tab-Page.zip . -x *.git* -x bin/\* -x .eslint*
+zip -r New-New-Tab-Page.zip . -x *.git* -x bin/\* -x .eslint* -x js-unminified/\*
